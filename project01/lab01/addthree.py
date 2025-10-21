804693779bec16c7113fb057783eaee7400ab654
Simon Lartey <slarte27@colby.edu>
2025-10-20 22:25:37 -0400

    Add Project 1 lab01: SIR simulation and helper functions

---
+++ project01/lab01/addthree.py
@@ -0,0 +1,13 @@
+'''Simon Lartey
+09/15/2023
+CS152B
+lab01
+this program prints the average and sum of three numbers'''
+print('version 1')
+print('sum', 42+21+6)
+print('average',(42+21+6)/3)
+
+def myfunction(a,b,c): 
+    print('sum',a+b+c)
+myfunction(4,5,6)
+
